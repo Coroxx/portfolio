524c1bbe5638a69650055d66070743614a6f2fc5
Coroxx <unnainstituteur@gmail.com>
2021-09-30 14:41:13 +0200

    📝 Fix typos

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,12 +21,12 @@
         <br />
         <br />
         Cela fait une paire d'année que je me suis intéressé au développement,
-        notamment le web depuis cette année. J'apprends par projet en
-        autodidacte, et ce qui me réussit le mieux. J'ai appris à analyser un
-        problème, à le décortiquer et à tirer profit de la solution. Quand on se
-        questionne sur un concept, ce n'est pas tellement l'arrivée qui est
-        importante mais le chemin parcouru et c'est pour ça que j'aime sortir de
-        ma zone de confort.
+        notamment web depuis 2021. J'apprends par projet en autodidacte, et ce
+        qui me réussit le mieux. J'ai appris à analyser un problème, à le
+        décortiquer et à tirer profit de la solution. Quand on se questionne sur
+        un concept, ce n'est pas tellement l'arrivée qui est importante mais le
+        chemin parcouru et c'est pour ça que j'aime sortir de ma zone de
+        confort.
       </p>
     </div>
   </div>
